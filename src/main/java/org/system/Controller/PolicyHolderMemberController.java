86bacc38a59e5e62f9daf27817ee0cf022996260
mintdunno <tnm1042002@gmail.com>
2024-05-10 08:41:31 +0700

    update member for policy

---
+++ src/main/java/org/system/Controller/PolicyHolderMemberController.java
@@ -0,0 +1,36 @@
+package org.system.Controller;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
+public class PolicyHolderMemberController {
+
+    @FXML
+    private FontAwesomeIconView addMember;
+
+    @FXML
+    private TableColumn<?, ?> editCol;
+
+    @FXML
+    private TableColumn<?, ?> emailCol;
+
+    @FXML
+    private TableColumn<?, ?> feeCol;
+
+    @FXML
+    private TableColumn<?, ?> idCol;
+
+    @FXML
+    private TableColumn<?, ?> nameCol;
+
+    @FXML
+    private TableColumn<?, ?> phoneCol;
+
+    @FXML
+    private FontAwesomeIconView refresh_button;
+
+    @FXML
+    private TableColumn<?, ?> usernameCol;
+
+}
