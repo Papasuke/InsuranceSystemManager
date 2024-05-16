bc9151c4aa1ae1a67e4f0f87f39442f1a7d4d3dc
MinT <tnm1042002@gmail.com>
2024-05-16 22:19:21 +0700

    update customerTestingUi

--- src/main/java/org/system/Application/customerTestingUi.java
+++ src/main/java/org/system/Application/customerTestingUi.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class customerTestingUi extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(customerTestingUi.class.getResource("/Fxml/Customer/PolicyHolderClaim.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(customerTestingUi.class.getResource("/Fxml/Customer/policyHolderDashBoard.fxml"));
         stage.initStyle(StageStyle.UNDECORATED);
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
