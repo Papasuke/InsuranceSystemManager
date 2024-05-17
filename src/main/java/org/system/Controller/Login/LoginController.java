13214e0284c16b576dd726369fb58a844ed754b3
MinT <tnm1042002@gmail.com>
2024-05-17 23:12:55 +0700

    fixed bug close everything when save

--- src/main/java/org/system/Controller/Login/LoginController.java
+++ src/main/java/org/system/Controller/Login/LoginController.java
@@ -3,9 +3,15 @@ package org.system.Controller.Login;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 //import org.system.DAO.Impl.AccountDAOImpl;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.system.Model.Account;
 import org.system.utils.SceneController;
 
@@ -62,9 +68,25 @@ public class LoginController {
             loggedInAccount = account;
             if(role == "POLICYHOLDER"){
                 try {
-                    SceneController.switchSceneCustomer(event, "policyHolderDashBoard");
+
+                    Parent parent = FXMLLoader.load(getClass().getResource("/Fxml/Customer/policyHolderDashBoard.fxml"));
+                    Scene newScene = new Scene(parent);
+
+                    // Get the current stage using the mainMenu AnchorPane
+                    Stage currentStage = (Stage) loginButton.getScene().getWindow();
+
+                    // Create the new stage and set the scene
+                    Stage newStage = new Stage();
+                    newStage.setScene(newScene);
+                    newStage.initModality(Modality.APPLICATION_MODAL); // Optional: Make the new stage modal if needed
+                    newStage.initStyle(StageStyle.UNDECORATED); // Optional: Set new stage style if needed
+                    newStage.show();
+
+                    // Close the current stage
+                    currentStage.close();
                     System.out.println("login success");
                     System.out.println(loggedInAccount.toString());
+
                 } catch (IOException e) {
                     // Handle the IOException here, e.g.,
                     System.err.println("Error switching scene: " + e.getMessage());
