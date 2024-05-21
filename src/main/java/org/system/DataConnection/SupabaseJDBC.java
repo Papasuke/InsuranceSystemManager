51d09a088cd710018f403adcecfa11436d795375
MinT <tnm1042002@gmail.com>
2024-05-21 15:36:03 +0700

    update main

--- src/main/java/org/system/DataConnection/SupabaseJDBC.java
+++ src/main/java/org/system/DataConnection/SupabaseJDBC.java
@@ -4,21 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SupabaseJDBC {
-    private static final String url = "jdbc:postgresql://aws-0-us-west-1.pooler.supabase.com:5432/postgres?user=postgres.nnbvogonnevqblzrnddt&password=Nmkk0934149833";
     private static final String urlMint = "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres?user=postgres.ximobxforphecpxrsmsd&password=Tranngocminh123";
-
-    public static Connection connect() throws SQLException {
-        return DriverManager.getConnection(url);
-    }
-
-    public static boolean testConnection() {
-        try (Connection connection = connect()) {
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
 
     public static Connection mintDatabase(){
         try{
--- src/main/java/org/system/Main.java
+++ src/main/java/org/system/Main.java
@@ -1,13 +1,27 @@
 package org.system;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.system.Application.testingApp;
 import org.system.DataConnection.SupabaseJDBC;
 
-public class Main {
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(testingApp.class.getResource("/Fxml/Login.fxml"));
+        stage.initStyle(StageStyle.UTILITY);
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Mint");
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) {
-        if (SupabaseJDBC.testConnection()) {
-            System.out.println("Connected to database");
-        } else {
-            System.out.println("Fail to connect to database");
-        }
+        launch(args);
     }
 }
